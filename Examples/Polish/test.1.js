5f9bff3371fd777ee6524ee630d51288cbccb7b9
Bob Delamater <bob.delamater@sage.com>
2018-10-21 20:59:04 -0700

    Using root command
    It worked once, but isn't working properly now. Need to troubleshoot, but this command gets us a bit farther.

---
+++ Examples/Polish/test.1.js
@@ -0,0 +1,27 @@
+var n = getNumber(2,5);
+var x = myFunc1(1);
+var y = myFunc1(2);
+var z = myFunc1(10);
+
+return;
+
+
+function getNumber(param1, param2)
+{
+    return param1 * param2;
+}
+function myFunc1(x)
+{
+    switch (x)
+    {
+        case 1:
+            return 1;
+        case 2:
+            return 2;
+        case 3:
+            return 3;
+        default: 
+            return 0;
+    }
+    return x;
+}
