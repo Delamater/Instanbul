bf1d139bfa1f1f55c09983092a40fb380c1310d6
Delamater <Bob.Delamater@sage.com>
2018-12-14 12:25:53 -0800

    Test commit #2 for Alfred

--- MakeTestFiles.ps1
+++ MakeTestFiles.ps1
@@ -1,5 +1,3 @@
 ﻿# Make N number of copies of the same test file
 $n = 10000
 1..$n | % { copy-Item "C:\WorkingFolder\Istanbul\Examples\Polish\test.js" "C:\WorkingFolder\Istanbul\Examples\Polish\ztest$_.js"}
-
-# END
